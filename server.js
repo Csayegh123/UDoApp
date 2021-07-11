c829915efffe0a214318170b843b3a7a4a50ae68
Christopher <sayegh.topher@gmail.com>
2021-07-10 20:03:13 -0500

    added port 3308

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3308;
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
