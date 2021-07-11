45bc1a1b69799237ff689369f3ef2f30960259ef
Christopher <sayegh.topher@gmail.com>
2021-07-10 20:25:00 -0500

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -11,9 +11,9 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'herokuapp',
+      host: 'localhost',
       dialect: 'mysql',
-      port: 3308
+      port: 3001
     }
   );
 }
