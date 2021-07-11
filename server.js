6b9c64a873208eedfd51aec4e4e7b84126d60230
Christopher <sayegh.topher@gmail.com>
2021-07-10 19:38:26 -0500

    updates port5001

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server is on`);
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
